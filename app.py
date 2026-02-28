e4b30beafc7c89d376d148e529ca438c2eb11400
Albert Antony <albertantonypy02@gmail.com>
2026-02-28 11:52:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ OPTIMAL_THRESHOLD = 0.25
 
 # --- 3. Streamlit App Layout ---
 st.set_page_config(page_title="Heart Failure Risk Prediction", layout="centered")
-st.title("❤️ Heart Failure Risk Prediction 🩺")
+st.title("Heart Failure Risk Prediction🩺")
 st.markdown("--- ")
 st.markdown("### Enter Patient's Clinical Parameters")
 st.write("Adjust the values below to predict the risk of a heart disease event.")
